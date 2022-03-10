144f87ac85d5895274b7e4a93291d6f8b45cc32b
andrestaffa <33270683+andrestaffa@users.noreply.github.com>
2022-03-09 19:04:22 -0700

    Dashboard & Discover Pages
    Completed first UI iteration of the Dashboard and Discover pages.

---
+++ MosaicFunds/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MosaicFunds.Core;
+
+namespace MosaicFunds.MVVM.ViewModel 
+{
+    class MainViewModel : ObservableObject 
+    {
+
+        public RelayCommand DashboardCommand { get; set; }
+        public RelayCommand DiscoverCommand { get; set; }
+        public RelayCommand NewsCommand { get; set; }
+        public RelayCommand TransactionsCommand { get; set; }
+        public RelayCommand SettingsCommand { get; set; }
+
+
+        public DashboardViewModel DashboardVM { get; set; }
+        public DiscoverViewModel DiscoverVM { get; set; }
+
+        private object currentView;
+
+        public object CurrentView 
+        {
+            get { return this.currentView; }
+            set { this.currentView = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel() {
+            this.DashboardVM = new DashboardViewModel();
+            this.DiscoverVM = new DiscoverViewModel();
+            this.CurrentView = DashboardVM;
+            relayCommanders();
+        }
+
+        private void relayCommanders() {
+            DashboardCommand = new RelayCommand(o => { this.CurrentView = this.DashboardVM; });
+            DiscoverCommand = new RelayCommand(o => { this.CurrentView = this.DiscoverVM; });
+            NewsCommand = new RelayCommand(o => { });
+            TransactionsCommand = new RelayCommand(o => { });
+            SettingsCommand = new RelayCommand(o => { });
+        }
+
+
+    }
+}
