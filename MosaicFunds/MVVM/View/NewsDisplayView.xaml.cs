9a7ad6b495393237f0edd7b244f5aa76b843d6b6
andrestaffa <33270683+andrestaffa@users.noreply.github.com>
2022-03-21 16:21:15 -0600

    News Display Page
    - Added a new sub-page to the News page that displays the actual article.

---
+++ MosaicFunds/MVVM/View/NewsDisplayView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MosaicFunds.MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MosaicFunds.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for NewsDisplayView.xaml
+    /// </summary>
+    public partial class NewsDisplayView : UserControl
+    {
+        public NewsDisplayView()
+        {
+            InitializeComponent();
+
+            MainViewModel mainViewModel = (MainViewModel)Application.Current.MainWindow.DataContext;
+            this.webView.Source = new Uri(mainViewModel.NewsDisplayViewModel.link);
+        }
+    }
+}
---
+++ MosaicFunds/MVVM/ViewModel/NewsDisplayViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaicFunds.MVVM.ViewModel
+{
+    class NewsDisplayViewModel
+    {
+
+        public string link = "";
+
+        public NewsDisplayViewModel() {
+
+        }
+
+    }
+}
