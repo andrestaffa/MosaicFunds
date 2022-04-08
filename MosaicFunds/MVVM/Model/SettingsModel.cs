43d652f0e3478ffe1d7dc9874145eb1c57b137ed
andrestaffa <33270683+andrestaffa@users.noreply.github.com>
2022-04-07 19:57:53 -0600

    Settings Logic Complete
    - Completed the logic of the settings page.
    - Each page has their own data.

--- MosaicFunds/MVVM/Model/SettingsModel.cs
+++ MosaicFunds/MVVM/Model/SettingsModel.cs
@@ -8,11 +8,31 @@ namespace MosaicFunds.MVVM.Model {
     class SettingsModel {
 
         public string Title { get; set; }
+        public string Settings1_Static { get; set; }
+        public string Settings1 { get; set; }
+        public string Settings2_Static { get; set; }
+        public string Settings2 { get; set; }
+        public string Settings3_Static { get; set; }
+        public string Settings3 { get; set; }
+        public string Settings4_Static { get; set; }
+        public string Settings4 { get; set; }
 
         public SettingsModel() {}
 
-        public SettingsModel(string title) {
+        public SettingsModel(string title, string settings1_static, string settings1, string settings2_static, string settings2, string settings3_static, string settings3, string settings4_static, string settings4) {
             this.Title = title;
+
+            this.Settings1_Static = settings1_static;
+            this.Settings1 = settings1;
+
+            this.Settings2_Static = settings2_static;
+            this.Settings2 = settings2;
+
+            this.Settings3_Static = settings3_static;
+            this.Settings3 = settings3;
+
+            this.Settings4_Static = settings4_static;
+            this.Settings4 = settings4;
         }
     }
 }
--- MosaicFunds/MVVM/View/SettingsView.xaml.cs
+++ MosaicFunds/MVVM/View/SettingsView.xaml.cs
@@ -33,13 +33,29 @@ namespace MosaicFunds.MVVM.View
             MainViewModel mainViewModel = (MainViewModel)Application.Current.MainWindow.DataContext;
 
             if (button == this.personalInfoButton) {
-                mainViewModel.SettingsInfoViewModel.settingsModel = new SettingsModel("Personal Information");
+                mainViewModel.SettingsInfoViewModel.settingsModel = new SettingsModel(title: "Personal Information", 
+                                                                                        settings1_static: "First Name", settings1: "Andre",
+                                                                                        settings2_static: "Last Name", settings2: "Staffa",
+                                                                                        settings3_static: "Email Address", settings3: "andre.staffa@ucalgary.ca",
+                                                                                        settings4_static: "Country", settings4: "Canada");
             } else if (button == this.bankingInfoButton) {
-                mainViewModel.SettingsInfoViewModel.settingsModel = new SettingsModel("Banking and Finance");
+                mainViewModel.SettingsInfoViewModel.settingsModel = new SettingsModel(title: "Banking and Finance",
+                                                                                        settings1_static: "Account Number", settings1: "*******0643",
+                                                                                        settings2_static: "Bank Number", settings2: "******",
+                                                                                        settings3_static: "Branch", settings3: "TD Bank",
+                                                                                        settings4_static: "Account Balance", settings4: "$517,436.43");
             } else if (button == this.securityButton) {
-                mainViewModel.SettingsInfoViewModel.settingsModel = new SettingsModel("Security");
+                mainViewModel.SettingsInfoViewModel.settingsModel = new SettingsModel(title: "Security",
+                                                settings1_static: "Password", settings1: "*********",
+                                                settings2_static: "Change Password", settings2: "",
+                                                settings3_static: "Enable Two-Factor Authentication", settings3: "",
+                                                settings4_static: "Login Access-Token", settings4: "*****489");
             } else if (button == this.personalizationButton) {
-                mainViewModel.SettingsInfoViewModel.settingsModel = new SettingsModel("Personalization");
+                mainViewModel.SettingsInfoViewModel.settingsModel = new SettingsModel(title: "Personalization",
+                                               settings1_static: "Change Theme", settings1: "Color Picker",
+                                               settings2_static: "Chart Customization", settings2: "Edit Scheme",
+                                               settings3_static: "Remember Login", settings3: "Enabled",
+                                               settings4_static: "Reset Settings", settings4: "");
             }
 
             mainViewModel.CurrentView = mainViewModel.SettingsInfoViewModel;
